ef8362b8d5033b3bbc233b70fe3ecc9b81cefaf6
@ahowe442 <howe.abigail@gmail.com>
2020-01-25 21:11:06 -0600

    fixed bug in Main these impacted Job and Location.

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -15,9 +15,9 @@ public class Location {
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
-    public Location(int id, String value) {
+    public Location(String value) {
+        this();
         this.value = value;
-        new Location();
     }
 
 
